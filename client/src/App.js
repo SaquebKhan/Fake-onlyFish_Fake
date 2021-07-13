7a8a762418a1b7a49ff8f9cb91464c4ec8872d50
Jamie Kaczor <dignanjk@aol.com>
2021-07-13 09:12:22 -0400

    reactpages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import MarinaContainer from "./components/MarinaContainer";
 
 function App() {
 
