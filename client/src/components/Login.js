b1c5af8f1c7b907893456992ad7969a8b4b1ba4d
Jamie Kaczor <dignanjk@aol.com>
2021-07-15 11:15:22 -0400

    signuplogin

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,23 @@
 import React from 'react'
 
-const Signup = () => {
+const Login = () => {
     return (
         <div className="App">
             <header className="App-header">
 
                 <h1>Only<span className="fish">Fish</span></h1>
                 <form>
-                    <input type="text" placeholder="Username" className="user-name" />
-                </form>
-                <form>
                     <input type="text" placeholder="E-mail" className="email-input" />
                 </form>
                 <form>
                     <input type="text" placeholder="Password" className="password-input" />
                 </form>
-                <button type="button">Signup</button>
+                <button type="button">LOGIN</button>
+                <p>Dont have an account yet?</p>
+                <a href="#" className="sign-up">Sign up for OnlyFish.com</a>
             </header>
         </div>
     )
 }
 
-export default Signup
-
+export default Login;
