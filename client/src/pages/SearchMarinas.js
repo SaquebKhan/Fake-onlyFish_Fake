f3b5f1aa83baf5ffa6cd49bb373095594d97a026
Jamie Kaczor <dignanjk@aol.com>
2021-07-18 13:10:41 -0400

    api/login/signup

--- client/src/pages/SearchMarinas.js
+++ client/src/pages/SearchMarinas.js
@@ -1,16 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import axios from 'axios';
+// import '.client/utils/api.js'
 
 const SearchMarinas = () => {
     
-    
-    
-    
-    
-    
-    
-    
+    const [marinas, setMarinas] = useState([])
+    const [search, setSearch] = useState('')
+    console.log(marinas)
+
+    useEffect(() => {
+        axios.get('https://api.marinas.com//v1/points/search?')
+            .then(res => {
+                setMarinas(res.data);
+            }).catch(err => console.log(err, 'error in setMarinas useEffect'))
+    }, [search])
+
+    const handleChange = async e => {
+        setSearch(e.target.value);
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+    }
+
+    const handleKeypress = e => {
+        //it triggers by pressing the enter key
+        if (e.keyCode === 13) {
+            handleSubmit();
+        }
+    };
+
+
+
+
+
+
     return (
         <div>
             <Jumbotron fluid className='text-light bg-primary'>
@@ -19,10 +44,11 @@ const SearchMarinas = () => {
                     <Form /*onSubmit={handleFormSubmit}*/>
                         <Form.Row>
                             <Col xs={12} md={8}>
-                                <Form.Control 
+                                <Form.Control
                                     name='searchInput'
                                     /*value={searchInput}*/
-                                    /*onChange={(e) => setSearchInput(e.target.value)}*/
+                                    onChange={handleChange}
+                                    onKeyPress={handleKeypress}
                                     type='text'
                                     size='lg'
                                     placeholder='Search for a Marina'
